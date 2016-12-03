72240b4d86537c85c3fb6706dafab1a375726e0c
Jonathan D'Alonzo <Jdalonz19@gmail.com>
2016-12-03 11:36:30 -0500

    Create QueueException.java

---
+++ QueueException.java
@@ -0,0 +1,10 @@
+/**
+ *
+ * @author dalonzoj8
+ */
+public class QueueException extends RuntimeException {
+
+  public QueueException(String s) {
+    super(s);
+  }  // end constructor
+}  // end QueueException
